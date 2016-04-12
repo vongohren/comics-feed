72d2a9294e038177b21e03f6c49a73faf874827f
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2016-04-12 22:38:11 +0200

    added feed as a dependency and now serves an xml of lunch

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
+var Feed = require('feed');
 var url = "http://www.dagbladet.no/tegneserie/lunch/"
-var cronTime = process.env.CRON_TIME || '00 58 20 * * 1-7';
+var cronTime = process.env.CRON_TIME || '00 39 22 * * 1-7';
 var timeZone = process.env.TIME_ZONE || 'Europe/Oslo';
 var CronJob = require('cron').CronJob;
 var job =  new CronJob({
@@ -11,14 +13,45 @@ var job =  new CronJob({
 	timeZone: timeZone
 });
 
+var lunchUrl = "";
+
 function fetchLunch() {
   request(url, function (error, response, body) {
     if (!error) {
       var $ = cheerio.load(body);
       var image = $('img.tegneserie').attr('src')
-      console.log(image);
+      lunchUrl = image;
     } else {
       console.log("We’ve encountered an error: " + error);
     }
   });
 }
+
+var app = express();
+
+app.get('/lunch', function (req, res) {
+  var feed = new Feed({
+    title:          'Lunch feed',
+    description:    'This is the norwegian lunch comic feed',
+    link:           'http://lunchstriper.no/',
+    image:          'http://lunchstriper.no/assets/graphics/logo.png',
+    copyright:      'None',
+    id: "01294149"
+  });
+  feed.addItem({
+    title: "Lunch",
+    link: lunchUrl,
+    description: "Lunchstripe",
+    date: new Date(),
+  })
+
+
+
+
+  res.set('Content-Type', 'text/xml');
+  res.send(feed.render('rss-2.0'));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
