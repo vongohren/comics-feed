8c5d7907d408fe58a0f3504381e985ba877badca
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2016-04-17 21:02:38 +0200

    added db and differentiation on the urls. Aswell as guid. Pushing up and deploying to test if its valid rss

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
-var Feed = require('feed');
+var feed = require('feed');
+var uuid = require('node-uuid');
 var url = "http://www.dagbladet.no/tegneserie/lunch/"
 var cronTime = process.env.CRON_TIME || '00 39 22 * * 1-7';
 var timeZone = process.env.TIME_ZONE || 'Europe/Oslo';
@@ -12,15 +13,42 @@ var job =  new CronJob({
 	start: true,
 	timeZone: timeZone
 });
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
+var Url = mongoose.model('Url', {
+  value: String,
+  date: { type: Date, default: Date.now },
+  uuid: { type: String, default: uuid.v4() }
+});
+
 
 var lunchUrl = "";
+
+fetchLunch();
 
 function fetchLunch() {
   request(url, function (error, response, body) {
     if (!error) {
       var $ = cheerio.load(body);
-      var image = $('img.tegneserie').attr('src')
-      lunchUrl = image;
+      var imageSrc = $('img.tegneserie').attr('src')
+      request(imageSrc, function (error, res, body) {
+        if (!error) {
+          var promise = Url.where("value").equals(res.request.href).exec();
+          promise.then(function(entries) {
+            if(entries.length == 0 ) {
+              console.log("INSIDE IF")
+              var newEntry = new Url({value:res.request.href})
+              newEntry.save(function (err, userObj) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log('saved successfully:', userObj);
+                }
+              });
+            }
+          })
+        }
+      })
     } else {
       console.log("We’ve encountered an error: " + error);
     }
@@ -30,7 +58,7 @@ function fetchLunch() {
 var app = express();
 
 app.get('/lunch', function (req, res) {
-  var feed = new Feed({
+  var lunchFeed = new feed({
     title:          'Lunch feed',
     description:    'This is the norwegian lunch comic feed',
     link:           'http://lunchstriper.no/',
@@ -38,18 +66,21 @@ app.get('/lunch', function (req, res) {
     copyright:      'None',
     id: "01294149"
   });
-  feed.addItem({
-    title: "Lunch",
-    link: lunchUrl,
-    description: "Lunchstripe",
-    date: new Date(),
-  })
-
-
-
-
-  res.set('Content-Type', 'text/xml');
-  res.send(feed.render('rss-2.0'));
+  var lastThreePromise = Url.find({}).sort('-date').limit(3).exec()
+  lastThreePromise.then(function(objs) {
+    console.log(objs)
+    for(var entry of objs) {
+      lunchFeed.addItem({
+        title: "Lunch",
+        link: entry.value,
+        guid: entry.uuid,
+        description: "Lunchstripe",
+        date: entry.date,
+      })
+    }
+    res.set('Content-Type', 'text/xml');
+    res.send(lunchFeed.render('rss-2.0'));
+  });
 });
 
 app.listen(3000, function () {
