c716b77192c4e1c0d8a489893e09949d45ad6410
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2016-04-17 21:13:54 +0200

    port fix

--- index.js
+++ index.js
@@ -82,7 +82,8 @@ app.get('/lunch', function (req, res) {
     res.send(lunchFeed.render('rss-2.0'));
   });
 });
+var port = process.env.PORT || 3000;
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port '+port);
 });
