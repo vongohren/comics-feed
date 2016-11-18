e4c3fe28d5980ad186d9ef9fe8a99445a9d97509
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2016-11-18 20:47:23 +0100

    added wumo to the stack

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ var express = require('express');
 var lunch = require('./modules/dagbladet/lunch')
 var pondus = require('./modules/dagbladet/pondus')
 var nemi = require('./modules/dagbladet/nemi')
+var wumo = require('./modules/heltnormalt/wumo')
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect(process.env.MONGODB_URI);
 
 lunch.init(process.env.LUNCH_HOUR || '11', process.env.LUNCH_MIN || '00')
 pondus.init(process.env.PONDUS_HOUR || '10', process.env.PONDUS_MIN || '00')
-nemi.init(process.env.PONDUS_HOUR || '12', process.env.PONDUS_MIN || '00')
+nemi.init(process.env.NEMI_HOUR || '12', process.env.NEMI_MIN || '00')
+wumo.init(process.env.WUMO_HOUR || '09', process.env.WUMO_MIN || '00')
 
 var app = express();
 app.get('/lunch', lunch.routeFunction);
 app.get('/pondus', pondus.routeFunction);
 app.get('/nemi', nemi.routeFunction);
+app.get('/nemi', wumo.routeFunction);
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
---
+++ modules/heltnormalt/wumo.js
@@ -0,0 +1,63 @@
+var name = 'wumo'
+var itemDescription = 'Wumostripe'
+var tegneserieLink = 'http://wumo.com/wumo'
+var tegneserieLogo = 'http://wumo.com/images/en_US/m_header_wumo.png'
+var url = 'http://heltnormalt.no/wumo'
+var Entry = require('../../models/comic-entry.js');
+var request = require('request');
+var cheerio = require('cheerio');
+var generateFeed = require('../../utils/generateFeed');
+
+
+exports.init = function(hour, minute) {
+  setupCronjob(hour, minute);
+  fetch();
+}
+
+function setupCronjob(hour, minute) {
+  var cronTime = process.env.CRON_TIME || '00 '+minute+' '+hour+' * * 1-7';
+  var timeZone = process.env.TIME_ZONE || 'Europe/Oslo';
+  var CronJob = require('cron').CronJob;
+  var job =  new CronJob({
+    cronTime: cronTime,
+    onTick: fetch,
+    start: true,
+    timeZone: timeZone
+  });
+}
+
+function fetch() {
+  request(url, function (error, response, body) {
+    if (!error) {
+      var $ = cheerio.load(body);
+      var imageSrc = $(".strip.wumo img").attr("src")
+      request(imageSrc, function (error, res, body) {
+        if (!error) {
+          var promise = Entry.where("url").equals(res.request.href).exec();
+          promise.then(function(entries) {
+            if(entries.length == 0 ) {
+              var newEntry = new Entry({url:res.request.href, label:name})
+              newEntry.save(function (err, userObj) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log('saved successfully:', userObj);
+                }
+              });
+            }
+          })
+        }
+      })
+    } else {
+      console.log("We’ve encountered an error: " + error);
+    }
+  });
+}
+
+exports.routeFunction = function (req, res) {
+  const obj = generateFeed(name, itemDescription, tegneserieLink, tegneserieLogo)
+  obj.then(function(feed){
+    res.set('Content-Type', 'text/xml');
+    res.send(feed);
+  })
+};
