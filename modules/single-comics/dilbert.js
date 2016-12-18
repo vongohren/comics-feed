700447495d012b73bfbdc0a33c36a2b649d6f6f6
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2016-12-18 23:06:24 +0100

    added dilbert

---
+++ modules/single-comics/dilbert.js
@@ -0,0 +1,21 @@
+var Feed = require('../feed');
+
+class Dilbert extends Feed {
+  constructor({
+    name = 'Dilbert',
+    itemDescription = 'Dilbertstripe',
+    tegneserieSideLink = 'http://dilbert.com/',
+    tegneserieLogo = 'http://dilbert.com/assets/dilbert-logo-4152bd0c31f7de7443b4bc90abd818da.png',
+    stripUrl = 'http://dilbert.com/',
+    hour = '10',
+    minute = '00'
+  }) {
+    super(name, itemDescription, tegneserieSideLink, tegneserieLogo, stripUrl, hour, minute);
+  }
+
+  extractImageSrc($) {
+    return $($('.comic-item-container')[0]).find('img').attr('src')
+  }
+}
+
+module.exports = Dilbert;
