ac614f4bd36aa33554674dcf78eae209988a291a
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2017-04-28 23:35:44 +0200

    added the slackbot to index, and bodyparsin so my slackbot logic works with slack api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var xkcd = require('./modules/single-comics/xkcd')
 var mongoose = require('mongoose');
+var Slackbot = require('./slackbot');
+var bodyParser = require('body-parser');
 const logger = require('./utils/logger');
 const comicsStorage = require('./comics');
 mongoose.connect(process.env.MONGODB_URI);
@@ -10,11 +12,16 @@ var comics = comicsStorage.available
 xkcd.init(process.env.XKCD_HOUR || '09', process.env.XKCD_MIN || '30')
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 for(var comic of comics) {
   app.get(`/${comic.name}`, comic.routeFunction.bind(comic));
 }
 
 app.get('/xkcd', xkcd.routeFunction);
+
+new Slackbot(app);
 
 var port = process.env.PORT || 3000;
 
