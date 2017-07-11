c8ac557a828ac5f578db5073e9c02ee970185ab0
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2017-07-12 01:30:25 +0200

    removed unused commands, updated query to match new strategy

--- slackbot/index.js
+++ slackbot/index.js
@@ -4,7 +4,6 @@ const interactiveHandler = require('./interactiveHandler');
 const Team = require('./models/slack-teams');
 const postToTeam = require('./poster').postToTeam;
 const initAgendaForTeam = require('./poster').initAgendaForTeam;
-const Agenda = require('./utils/agenda');
 
 class Slackbot {
     constructor(app) {
@@ -12,6 +11,15 @@ class Slackbot {
       this.initializeRoutes();
       this.clientId = process.env.SLACK_CLIENT_ID;
       this.clientSecret = process.env.SLACK_CLIENT_SECRET;
+      this.initAgendaForAllTeams();
+    }
+
+    initAgendaForAllTeams() {
+        Team.find({}, function(err, teams) {
+          teams.forEach(function(team) {
+            if(team.active) initAgendaForTeam(team)
+          });
+        });
     }
 
     initializeRoutes() {
@@ -22,40 +30,17 @@ class Slackbot {
         this.app.get('/slack', function (req, res) {
             res.sendFile(path.join(__dirname+"/slack.html"));
         });
-        this.app.post('/command', function(req, res) {
-            const promise = Team.where("team_id").equals(req.body.team_id).exec();
-            promise.then(function(teams) {
-                const team = teams[teams.length-1]
-                const subscriptions = team.subscriptions;
-                for(const subscription of subscriptions) {
-                        subscription.lastPublished = null
-                }
-                var query = { team_id: team.team_id };
-                Team.update(query, { subscriptions: subscriptions }, (err, raw) => {
-                    if (err) logger.log('error' `Team subscription update error: ${err}`)
-                    console.log('The raw response from Mongo was ', raw);
-                })
-            });
-            res.send('Cleared subscription posting');
-        });
         this.app.post('/interactive', function(req, res) {
             const body = JSON.parse(req.body.payload)
-            var promise = Team.where("team_id").equals(body.team.id).exec();
+            const text = body.original_message.text
+            const channel_id = text.slice(text.indexOf('#')+1,text.indexOf('>'))
+            const query = {team_id: body.team.id, "incoming_webhook.channel_id": channel_id }
+            var promise = Team.findOneAndUpdate(query, {active:true}).exec()
             //TODO: Skummelt med promisset, silent failing på alt inne i then
-            promise.then(function(teams) {
-                body.team = teams[teams.length-1]
+            promise.then(function(team) {
+                body.team = team
                 interactiveHandler(body, res)
             })
-        })
-
-        this.app.post('/cron', function(req, res) {
-            initAgendaForTeam(req.body.team_id)
-        })
-
-
-
-        this.app.post('/comics', function(req, res) {
-            postToTeam(req.body);
         })
     }
 }
