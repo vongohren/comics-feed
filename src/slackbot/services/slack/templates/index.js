e4e3667f2dc71e36a8a020aef7fd4bf6bc826f55
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2017-09-09 16:24:58 +0200

    Damn, return

--- src/slackbot/services/slack/templates/index.js
+++ src/slackbot/services/slack/templates/index.js
@@ -101,9 +101,7 @@ const selectRightOrderForAttachments = (team, subscriptionAttachment) => {
 const mapSubscriptionsToAttachmentsWithButtons = (team) => {
   return team.subscriptions.map(subscription=> {
     const matchingComic = comics.available.find(comic => {
-      console.log(comic)
-      console.log(subscription)
-      comic.name == subscription.name
+      return comic.name === subscription.name
     })
     return {
       "title": `${subscription.name.upperCaseFirstLetter()} - (${matchingComic.language.upperCaseFirstLetter()})`,
