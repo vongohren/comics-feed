9869bd9b3d63fe666af8411bf80c69c7907f7096
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2021-04-26 01:11:02 +0200

    Feature: more metadata

--- src/modules/dagbladet/dagbladet.js
+++ src/modules/dagbladet/dagbladet.js
@@ -4,7 +4,8 @@ class Dagbladet extends Feed {
   constructor(name, itemDescription, tegneserieSideLink, tegneserieLogo, stripUrl, hour, minute) {
     super(name, itemDescription, tegneserieSideLink, tegneserieLogo, stripUrl, hour, minute);
     this.mediator = 'Dagbladet';
-    this.mediatorLogo = 'http://i.imgur.com/2tvk2Io.jpg'
+    this.mediatorLogo = 'http://i.imgur.com/2tvk2Io.jpg',
+    this.mediatorUrl = 'https://www.dagbladet.no/tegneserie/nemi/'
   }
 }
 
--- src/modules/dagbladet/nemi.js
+++ src/modules/dagbladet/nemi.js
@@ -8,10 +8,14 @@ class Nemi extends Dagbladet {
     tegneserieLogo = '//vignette3.wikia.nocookie.net/cartoonfatness/images/3/3e/Blog_yk_4915575_7599245_tr_logo.png/revision/latest?cb=20140520195208',
     stripUrl = 'http://www.dagbladet.no/tegneserie/nemi/',
     hour = '10',
-    minute = '00'
+    minute = '00',
+    author = 'Lise Myhre',
+    authorUrl = 'https://en.wikipedia.org/wiki/Lise_Myhre'
   }) {
     super(name, itemDescription, tegneserieSideLink, tegneserieLogo, stripUrl, hour, minute);
     this.language = 'norwegian'
+    this.author = author
+    this.authorUrl = authorUrl
   }
 }
 
