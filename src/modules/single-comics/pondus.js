0f95f78013a84a29591996e5c3d7e182ca55888b
Snorre Lothar von Gohren Edwin <snorre.lothar.von.gohren.edwin@entur.org>
2023-09-03 10:43:12 +0200

    fix: pondus neede fresh authentication
    Added an error logger if it starts to go out

--- src/modules/single-comics/pondus.js
+++ src/modules/single-comics/pondus.js
@@ -3,6 +3,7 @@ var moment = require('moment');
 const axios = require('axios');
 const {Storage} = require('@google-cloud/storage');
 const logger = require('../../utils/logger')
+const jwt_decode = require('jwt-decode');
 
 // Instantiate a storage client
 const base64Decoded = Buffer.from(process.env.STORAGE_KEY, 'base64').toString('utf8')
@@ -35,14 +36,23 @@ class Pondus extends Feed {
   async extractImageSrc($,callback) {
     const today = moment().format('YYYY-MM-DD')
     const url = `https://www.vg.no/tegneserier/api/images/pondus/${today}`;
+    const oneYearJwt = "eyJraWQiOiIzYWNkODdmNS0zYmQyLTQ5ZTMtYWFhOC0wYTI5ZjU2MzY1YTIiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJzZHJuOnNwaWQubm86Y2xpZW50OjRlZjFjZmIwZTk2MmRkMmUwZDhkMDAwMCIsInN1YiI6IjgzZDNhMDY1LWNhZGMtNTAzMC1hYTdmLTgyY2I4OGEzYTc3ZiIsInVzZXJfaWQiOiIyNDEyMjAiLCJpc3MiOiJodHRwczpcL1wvc2Vzc2lvbi1zZXJ2aWNlLmxvZ2luLnNjaGlic3RlZC5jb20iLCJleHAiOjE3MjUyNjUxMjMsImlhdCI6MTY5MzcyOTEyMywianRpIjoiMmFlZDA3YzktM2VhYi00OWM3LTlkYmUtMzJlZjY2ZDcxY2M2In0.T_oZB3pgvmZlBOQkEUOD9VqfMvmLfzCEErGGSaqVPWsmAdvaafasc5YF61qRVKJgr57b03N3QJpkqCKBqZu-nI4GRtTqDygP5gfvXDXWg3-qWzZSIeKSdMdWPtY_5CdxEJL3iR1P_wEWboqufeQjftyCPWBMQalNBG5XUC_6AzADeyTHvNjZo_K9FbycYjTjACEH6xBsy1zqjrKV-8R1JMoUzZ-H8HFYL3scpeUgEAn6WOTcXNwG7rMQ9w9N3zC73vGA6CILUjncaZ63WaemUddSl5pO2ionD_LpcFWH62uD5I9fiasWWREgCUVtbkTG4Zzzy5PcXsA57f0Yek5AXklV2xRXxleXATKW9vJOGszFqdXkluOUUNZSeMID3xwhf1ZZtNipLYoMUeVsiYL62raYa0-84AxzL7M34yOWRy3r8IJK4zzV-N5qzff2nAbE4g_HBldS8jDI9ktNN4T737wOs1rNHFwdWW9XcixDb6nxQs58pncmvpc3u-BUqutmlQPA7_xenGrK3R-i9rEjknQq65pWLKI5m1UATFsHwsotzj-OW2j_H7iYORYFSfGAtT2bsfDatGQJJcJtWw-_lYTR7eZaTbsbmrUmEdTKAeCQxSXo86rFewtshLl8npQWZE27o583gCYPxmUk9uvsfy2xSwRNv4sxtzyP20bIbD0";
+    const jwtData = jwt_decode(oneYearJwt);
+    const exp = jwtData.exp;
+    const now = moment().unix();
+    // make a check if the jwt has 60 days left of expiring and error log it
+    if(exp - now < 5184000) {
+      logger.log('error', `Pondus jwt has less than 60 days left of expiring. Exp: ${exp} Now: ${now}. Just go to VG and load ${url}, login and copy the jwt from the cookie and replace it in the code.`)
+    }
+
     try {
       const res = await axios({
         method: 'get',
         url: url,
         responseType: 'stream',
         headers: {
-          Cookie: "clientBucket=11;_MBL={'u':'KDZdGELI5U','t':1661107210};id-jwt=eyJraWQiOiIzYWNkODdmNS0zYmQyLTQ5ZTMtYWFhOC0wYTI5ZjU2MzY1YTIiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJzZHJuOnNwaWQubm86Y2xpZW50OjVhOTgxMDU3YWM0YWNlNGFlMzNkY2Y1YyIsInN1YiI6IjgzZDNhMDY1LWNhZGMtNTAzMC1hYTdmLTgyY2I4OGEzYTc3ZiIsInVzZXJfaWQiOiIyNDEyMjAiLCJpc3MiOiJodHRwczpcL1wvc2Vzc2lvbi1zZXJ2aWNlLmxvZ2luLnNjaGlic3RlZC5jb20iLCJleHAiOjE2OTM1NDkyODYsImlhdCI6MTY2MjAxMzI4NiwianRpIjoiNDQwMjY0NzktNGYxOS00OTMzLTk5YTQtZGEzMjMzNzI2NTA2In0.YdOeT1lF9UbI00UXVzCGQYYxzbIE3yISiUZehwfDx_lCKne--4rqg7Qi4Uze133ompcyJRYaGcTCZcE0NbDC8fwgsxKmfdKwKG6BUYu68qJDel1iLV2p3RNYj7vpYqxjmuznLWGEISDihtxm2rT0L7ikIsvK7lHyOX050uGiHRVyJA3wfv28nVAR80AB8PPmp_o1KINbF_t093s_lA6KO_k7Ytn7WDHFPJzgFq91arSdANvvc1ahN1WAJHWA-21YcYeR7Er2zjgIXMroj_MXvDoojxh-MOy04VgB8MGRe1LWUts5vyo6rcPNfCA4MFEdUQEC_e73eNZLjg_pgKViifLxsFEkQuQL-EeIAA7mRWbIoHLfcnPJQQkzdffmGJ8uSmZsEPfkY-sGjZxQZvgQUJqPjs2A5LNKJbcS1FbSZIUmVFJ-2fL0nN_Is_9Wk8m5Bw0eR5WGqUd7W-zLZYEgPfDzLdeH6DtHz3ALl_e0MxtwlQRfb74LtFc1Q3WFBpqwa1p16JTQx8kxG2jMFVRDvAv9RF4ADZflg5nOQzIOKERLmCoPPSkOqGrWVxUYTwtRqHdPncOAHqr6pFoBlHC4kEB6td_lUj_1VGnQRm_Iw_REbQXnvJmaeiYL5iXoeWpv2aTb169rHm26arduD1P3ov6bGxNv-bHOrzD1oPNI5MA;SP_ID=eyJjbGllbnRfaWQiOiI0ZWYxY2ZiMGU5NjJkZDJlMGQ4ZDAwMDAiLCJhdXRoIjoiVUppWEtvV0xhOGYzREFMMnRPaGpnUnBDa0I3dHBSdlU3WUFyaWtyNzZSTnhVdkJSd2VHQjhnVEZtdnN6RWtXZmUtdV9IUU5reHdQajZhRWlTbm1iYS1UTV8tTmN6VUVwcnU1MUxzWXJSVzgifQ;_pulse2data=2fa395b0-432d-4910-865d-78f7091ffef7,v,241220,1662014187591,eyJpc3N1ZWRBdCI6IjIwMTgtMDYtMjZUMDk6NDJaIiwiZW5jIjoiQTEyOENCQy1IUzI1NiIsImFsZyI6ImRpciIsImtpZCI6IjIifQ..i7nrUdUGVS8zl-0PfXdPAg.X9indU9ZhERtjXPD0cy-QDEGgaeTVnAzVWy_nIkwYGNsFZLJsGYVnblb7aJJpcQ1rJ6fL1l06Cvhmn3ibXXVRerefXwz_b9WAA3eJ0ZPfrQidiGJS-Q6ZbuhNqEOhwn1LHPEDqPT5Ndrq1DTasdqbksmzSdYl4Gy5e9QF0hD4_W6Zpvw6Ki7Knf9JdWu5URm28fCvKuKVtN-2p723Tr_PWgldo20gkvdKIxa8XcFbNdZ4SGh_1e4ra0DjnoeocZvn0Rt9Nz1nCfx8vMNggzBb_D8o7oM16Da2Su2vz0g7mw.ZENoc3mxhZ7beq5yCySzmg,4971052420334917281,1662027687591,true,,eyJraWQiOiIyIiwiYWxnIjoiSFMyNTYifQ..lJeM-ZqRICMoLq3Dsy3hSBfg8u8mFZmvf5BXa5Wu-Q4;__mbl={'u':[{'uid':'g3We5WT0wCqb6lCI','ts':1662013410},1662103410]};_pulsesession=['sdrn:schibsted:session:531f4050-5977-4c71-9d30-ad18e6930231',1662013287600,1662013412485];VPW_State=dHVwbGVzPSgoOTk4LDE2NjIwMTM1NjYpKSxzaWc9MHgyYTA5MWM2YTA3MDViNDg4ZWEzY2ZmY2IyZDI2NTVlNjEzMTNlMDliNmUwMjM3Mzc3YTY1MjBkN2RmNTZhNDUw",
-          'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+          Cookie: `_ga=GA1.1.219148325.1688909103; _ga_DKKWD4LFQF=GS1.1.1689428492.2.1.1689428599.0.0.0; clientBucket=88; iter_id=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhaWQiOiI2NGYzNGQzMTlkZjhjYzAwMDFiYTI0MjQiLCJjb21wYW55X2lkIjoiNjA5NGVhMjQxNmQ1YjUwMDAxNWM2NDdlIiwiaWF0IjoxNjkzNjY2NjA5fQ._VOlG2GKUhrZ7RBQhh6Q0OD1TRLb1nXWgUxZ_s1UFBE; id-jwt=${oneYearJwt}`,
+          'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
         }
       })
 
